2b649ba5d5b854d292b1a6281de1858c7ba94a37
jfableon <jfableon@gmail.com>
2023-11-18 20:13:38 -0500

    Final diseño

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include __DIR__ . '/vendor/autoload.php';
 $pagina = $_REQUEST['p'] ?? 'inicio';
 $params = [
 	'menuItems' => [
-		(object) ['pag'=>'acercade', 'txt'=> 'Acerca de', 'icon'=>'fa-solid fa-user-tag'],
+		(object) ['pag'=>'acercade', 'txt'=> 'Trayectoria', 'icon'=>'fa-solid fa-user-tag'],
 		(object) ['pag'=>'proyectos','txt'=> 'Proyectos', 'icon'=>'fa-solid fa-diagram-project'],
 		(object) ['pag'=>'contacto', 'txt'=> 'Contacto' , 'icon'=>'fa-solid fa-address-card'],
 	],
